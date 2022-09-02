7f4a15f1bf661462eb4b48831caace0de3c42f32
Nahid-Hasan_Sourav <nahidhasansourav05@gmail.com>
2022-09-03 03:43:01 +0600

    generate dynamic url for category

--- JS/app.js
+++ JS/app.js
@@ -1,4 +1,4 @@
-console.log("Working");
+
 
 const loadCategory= async () =>{
     const url=' https://openapi.programming-hero.com/api/news/categories'
@@ -17,21 +17,21 @@ const loadCategory= async () =>{
 }
 
 const displayCategoryItem = (category) =>{
-    console.log("category : ",category);
+    // console.log("category : ",category);
 
     const categoryUl=document.getElementById("category-ul");
 
     
 
     category.data.news_category.forEach((item) =>{
-        console.log("item : ",item);
-        console.log("item : ",item.category_name);
+        // console.log("item : ",item);
+        // console.log("item : ",item.category_name);
         
         const li=document.createElement("li");
         li.classList.add("nav-item");
 
         li.innerHTML=`
-        <a href="#" class="fw-bold nav-link mx-3 fs-5" onclick="">${item.category_name}</a>
+        <a href="#" class=" nav-link mx-3 fs-5" onclick="selectCategoryItemByid(${item.category_id})">${item.category_name}</a>
         `
 
         categoryUl.appendChild(li);
@@ -41,5 +41,40 @@ const displayCategoryItem = (category) =>{
     })
   
 }
+
+
+const selectCategoryItemByid = (category_id) => {
+
+
+    console.log("After Item Click: " + category_id);
+    loadCategoryItemByid(category_id);
+
+}
+
+const loadCategoryItemByid= async (category_id) =>{
+    const url = `https://openapi.programming-hero.com/api/news/category/0${category_id}`;
+    console.log(url);
+
+    try{
+        const res = await fetch(url);
+        const data= await res.json();
+        // console.log("Display Category Item by ID : " , data);
+        displayCategoryItemByID(data);
+    }
+
+    catch(error){
+        console.log("Error : " + error);
+        console.log("Error : " + error.message);
+    }
+}
+
+
+const  displayCategoryItemByID=(data)=>{
+    console.log("Display Category Item by ID : " , data);
+}
+
+
+
+
 
 loadCategory();

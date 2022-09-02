255b7c0310e3ded0ed0175c94bf546e654f89cb9
Nahid-Hasan_Sourav <nahidhasansourav05@gmail.com>
2022-09-03 02:40:31 +0600

    added category item from api

--- JS/app.js
+++ JS/app.js
@@ -19,8 +19,23 @@ const loadCategory= async () =>{
 const displayCategoryItem = (category) =>{
     console.log("category : ",category);
 
+    const categoryUl=document.getElementById("category-ul");
+
+    
+
     category.data.news_category.forEach((item) =>{
         console.log("item : ",item);
+        console.log("item : ",item.category_name);
+        
+        const li=document.createElement("li");
+        li.classList.add("nav-item");
+
+        li.innerHTML=`
+        <a href="#" class="fw-bold nav-link mx-3 fs-5" onclick="">${item.category_name}</a>
+        `
+
+        categoryUl.appendChild(li);
+       
 
 
     })

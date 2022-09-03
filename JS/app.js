576d85debbb8be9452cf205a379bcceb32b063a8
Nahid-Hasan_Sourav <nahidhasansourav05@gmail.com>
2022-09-03 15:48:30 +0600

    successfully added modal

--- JS/app.js
+++ JS/app.js
@@ -43,14 +43,44 @@ const displayCategoryItem = (category) =>{
 }
 
 
+const toggleSpiner = (isLoading) => {
+    const spinner=document.getElementById('spinner-loader');
+    if(isLoading){
+        spinner.classList.remove('d-none');
+        console.log("loading show")
+    }
+
+    else{
+        spinner.classList.add('d-none');
+        console.log("remove it")
+    }
+    
+}
+
+
 const selectCategoryItemByid = (category_id) => {
 
-    
-    
+    toggleSpiner(true);
+   
     console.log("After Item Click: " + category_id);
     loadCategoryItemByid(category_id);
+    
 
 }
+
+// const toggleSpiner = (isLoading) => {
+//     const spinner=document.getElementById('spinner-loader');
+//     if(isLoading){
+//         spinner.classList.remove('d-none');
+//         console.log("loading show")
+//     }
+
+//     else{
+//         spinner.classList.add('d-none');
+//         console.log("remove it")
+//     }
+    
+// }
 
 const loadCategoryItemByid= async (category_id) =>{
   
@@ -73,7 +103,7 @@ const loadCategoryItemByid= async (category_id) =>{
 
 const  displayCategoryItemByID=(data)=>{
     console.log("Display Category Item by ID : " , data);
-
+    // toggleSpiner(true);
     console.log("Length : " , data.data.length);
     // console.log("Length : " , parentRow.children.length);
     const cardFoundNumber=document.getElementById("card-found-number");
@@ -83,7 +113,10 @@ const  displayCategoryItemByID=(data)=>{
      }
      else if(length===0){
          cardFoundNumber.innerText="Sorry!! No Card Found!!!";
+         toggleSpiner(false);
      }
+
+     
 
     const parentRow=document.getElementById("parent-card-row");
     // console.log(parentRow)
@@ -91,20 +124,26 @@ const  displayCategoryItemByID=(data)=>{
 
     // const parentColumn=document.getElementById("parent-colums");
    console.log(data.data);
+
+   const sortData = data.data.sort((a, b) => {
+    return a.total_view - b.total_view;
+    });
+    const reverseSortData = sortData.reverse();
+
      
      data.data.forEach((item)=>{
         const date=item.author.published_date;
         // console.log("length of date ",date.slice(0,10));
         
-        console.log("Category Item by   ID : " , item);
+        // console.log("Category Item by   ID : " , item._id);
        
         const div=document.createElement("div");
         div.classList.add("col");
 
         div.innerHTML=`
-        <div class="row gx-0 shadow" id="added-card-row">
+        <div class="row gx-0 shadow" id="added-card-row" onclick="ModalGenerate('${item._id}')" data-bs-toggle="modal" data-bs-target="#exampleModal" >
         <div class="col-lg-4 col-md-4 col-sm-12">
-            <img src="${item.image_url}" class="img-fluid w-100">
+            <img src="${item.image_url}" class="img-fluid p-3">
         </div>
         <div class="col-lg-8 col-md-8 col-sm-12">
             <div class="p-3">
@@ -115,8 +154,8 @@ const  displayCategoryItemByID=(data)=>{
             <div class="row gx-0">
             <div class="col-3    p-3">
                <div class="author d-flex align-items-center justify-content-center">
-                <div class="author-image w-25">
-                    <img src="${item.author.img}" id="author-images" class="w-100 d-block  rounded-circle">
+                <div class="author-image">
+                    <img src="${item.author.img}" id="author-images" class="d-block  rounded-circle">
                 </div>
                <div class="author-writter-date ps-3">
                 <p class="mb-0">${item.author.name ? item.author.name :"N/A"}</p>
@@ -128,7 +167,8 @@ const  displayCategoryItemByID=(data)=>{
                 <div class="view  d-flex align-items-center h-100 justify-content-center">
                     <div class=" d-flex h-50 justify-content-center align-items-center" id="view-icon">
                     <p class="me-1"><i class="fa-regular fa-eye"></i></p>
-                    <p class="ms-1">1.7 M</p>
+                    <p class="ms-1 fw-bold">${item.total_view ? item.total_view : "No View"
+                    }</p>
                     </div>
                 </div>
             </div>
@@ -167,14 +207,68 @@ const  displayCategoryItemByID=(data)=>{
     </div>
         
         `
-        parentRow.appendChild(div);
 
+
+      
+
+        // toggleSpiner(false);
+        parentRow.appendChild(div);
+        toggleSpiner(false);
+        
         // console.log("Length : " , parentRow.children.length);
         // length=parentRow.children.length;
 
 
     })
 
+}
+
+const generateModalCardItemByid= async (category_id) =>{
+  
+    const url = ` https://openapi.programming-hero.com/api/news/${category_id}`;
+    console.log(url);
+    
+    try{
+        const res = await fetch(url);
+        const data= await res.json();
+        console.log("Display Category Item by ID : " , data);
+       displayModalCard(data);
+    }
+
+    catch(error){
+        console.log("Error : " + error);
+        console.log("Error : " + error.message);
+    }
+}
+
+const  displayModalCard =(data)=>{
+    // console.log("Modal Data : ", data.data[0].author.name);
+    console.log("Modal Data : ", data.data[0]);
+
+    const modalBody=document.getElementById("modal-body");
+    modalBody.innerHTML='';
+
+    const div=document.createElement("div");
+    div.classList.add(".card");
+
+    div.innerHTML=`
+    
+    <img src="${data.data[0].image_url}" class="img-fluid">
+    `
+    modalBody.appendChild(div);
+}
+
+
+
+const ModalGenerate = (id) =>{
+    console.log("Modal ID: ",id);
+
+    generateModalCardItemByid(id);
+
+    // const url = `https://openapi.programming-hero.com/api/news/category/${id}`;
+    // console.log(url);
+    
+   
 }
 
 
